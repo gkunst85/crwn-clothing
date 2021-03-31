8b72966dbc69a72f91971b7fc9555a69747ffb89
Gur Kunst <gurku@payoneer.com>
2021-03-31 11:52:30 +0300

    update our menu-items to navigate withRouterHOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -45,8 +45,8 @@ class Directory extends React.Component {
 
         return (
             <div className="directory-menu">
-                {sections.map(({ title, imageUrl, id, size }) => (
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                {sections.map(({ id, ...sectionProps }) => (
+                    <MenuItem key={id} {...sectionProps} />
                 ))}
             </div>
         )
